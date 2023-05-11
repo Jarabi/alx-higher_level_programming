3a31371a90949051cd403d893d56957c1bcd4340
Alex Jarabi <jarabialex.gmail.com>
2023-05-11 08:43:30 -0700

    Import functions and run them

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -2,8 +2,13 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
+
+_sum = add(a, b)
+_diff = sub(a, b)
+_prod = mul(a, b)
+_quot = div(a, b)
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, _sum))
+    print("{:d} - {:d} = {:d}".format(a, b, _diff))
+    print("{:d} * {:d} = {:d}".format(a, b, _prod))
+    print("{:d} / {:d} = {:d}".format(a, b, _quot))
