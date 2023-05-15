ac46f2aec2179e716296488e5f85f380208f7a26
Alex Jarabi <jarabialex.gmail.com>
2023-05-15 15:43:29 -0700

    A function that adds two tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,18 +9,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Return:
         Tuple with the summed up elements
     """
-    lst_a = [0, 0]
-    lst_b = [0, 0]
-
-    check_tuple(lst_a, tuple_a)
-    check_tuple(lst_b, tuple_b)
-
-    a = lst_a[0] + lst_b[0]
-    b = lst_a[1] + lst_b[1]
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if len_a < 2:
+        if len_a == 1:
+            tuple_a = tuple_a[0], 0
+        else:
+            tuple_a = 0, 0
+    if len_b < 2:
+        if len_b == 1:
+            tuple_b = tuple_b[0], 0
+        else:
+            tuple_b = 0, 0
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
 
     return (a, b)
-
-
-def check_tuple(lst, tupl):
-    for idx, el in enumerate(tupl):
-        lst[idx] = el
