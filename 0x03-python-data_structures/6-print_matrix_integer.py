686f18008ccc4b0ea007196dfaf9b6eb816bab54
Alex Jarabi <jarabialex.gmail.com>
2023-05-15 05:54:25 -0700

    A function that prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers
+
+    Args:
+        matrix: A 2-dimensional array
+    Return:
+        Nothing
+    """
+    for outer in matrix:
+        for k in range(0, len(outer)):
+            print("{}".format(outer[k]), end="")
+
+            if k == 2:
+                continue
+            print(" ", end="")
+        print()
