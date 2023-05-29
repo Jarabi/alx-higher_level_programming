24e5c3524ae712792dda5e6de70c6c985b23bb8e
Alex Jarabi <jarabialex.gmail.com>
2023-05-29 12:43:11 -0700

    A function that prints an integer.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     """Prints an integer.
+
     Args:
         value: Value to be printed
     """
     try:
-        int(value)
         print("{:d}".format(value))
         return (True)
     except ValueError:
