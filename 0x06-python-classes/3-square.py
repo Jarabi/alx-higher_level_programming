34c5888bbcd6e60601618d7b92f87e7c3fcf7f98
Alex Jarabi <jarabialex.gmail.com>
2023-05-31 15:02:52 -0700

    A class Square that defines a square and returns square area.

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+'''Square class definition'''
+
+
+class Square:
+    '''Definition of a square based ona private instance attribute'''
+    def __init__(self, size=0):
+        '''Attribute definition
+
+        Args:
+            size(int): Size of the square
+        '''
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        '''Class method area
+
+        Returns:
+            Current square area
+        '''
+        return self.__size * self.__size
