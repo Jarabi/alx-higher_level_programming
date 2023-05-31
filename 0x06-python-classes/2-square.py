0bb2a517055e541dede85465cd6a55ef85ba4cee
Alex Jarabi <jarabialex.gmail.com>
2023-05-31 14:53:21 -0700

    A class Square that defines a square by size validation.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,12 +11,10 @@ class Square:
         Args:
             size(int): Size of the square
         '''
-        try:
-            isinstance(size, int)
-
-            if size < 0:
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
                 raise ValueError("size must be >= 0")
-            self.__size = size
-
-        except TypeError:
-            print("size must be an integer")
+        else:
+            raise TypeError("size must be an integer")
