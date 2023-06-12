28b606ea8c8e44b381951e445b018fdfe24947cf
Alex Jarabi <jarabialex.gmail.com>
2023-06-12 11:21:11 -0700

    Public instance that raises an exception.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    Unimplemented class
+'''
+
+
+class BaseGeometry:
+    '''An unimplemented class that raises an exception
+
+    Raises:
+        Exception: area() is not implemented.
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
