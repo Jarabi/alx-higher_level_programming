5620c0630c305d7b17e7e7e159bbe48645d35b40
Alex Jarabi <jarabialex.gmail.com>
2023-06-12 14:44:04 -0700

    A Rectangle class that implements area.

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''
+    Rectangle class
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+        Implements a rectangle class. Inherits from BaseGeometry
+    '''
+    def __init__(self, width, height):
+        '''Validates argument and instantiates variables
+
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        '''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def print(self):
+        '''Prints rectangle description'''
+
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def __str__(self):
+        '''Returns rectangle description'''
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        '''Returns the area of rectangle'''
+
+        return (self.__width * self.__height)
