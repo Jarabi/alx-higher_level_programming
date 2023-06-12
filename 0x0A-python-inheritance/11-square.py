aeafc451e6f9de97c3dc97ee9fdeb250a1e1530f
Alex Jarabi <jarabialex.gmail.com>
2023-06-12 15:36:21 -0700

    Implement a square class with __str__.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+    Square class
+'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+        Square class that inherits from Rectangle
+    '''
+    def __init__(self, size):
+        '''Instantiate with size
+
+        Args:
+            size: length of square side
+        '''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def area(self):
+        '''
+            Return area of square
+        '''
+        return (self.__size ** 2)
+
+    def __str__(self):
+        '''
+            Returns description of square
+        '''
+        return "[{}] {}/{}".format(
+                type(self).__name__,
+                self.__size,
+                self.__size
+        )
