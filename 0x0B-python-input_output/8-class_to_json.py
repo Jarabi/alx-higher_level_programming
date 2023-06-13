fa1857ced118fcdc5acdde7a905c32b342479030
Alex Jarabi <jarabialex.gmail.com>
2023-06-13 15:08:25 -0700

    A function that returns the dictionary description with simple data structure for JSON serialization of an object.

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,4 +9,4 @@ def class_to_json(obj):
         Returns the dictionary description with simple data structure
         JSON serialization of an object
     '''
-    return (getattr(obj, '__dict__'))
+    return (vars(obj))
