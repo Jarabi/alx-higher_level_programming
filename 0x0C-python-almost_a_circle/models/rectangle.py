2d30e978c66cda1fb96fa5eacb1dc84e35401373
Alex Jarabi <jarabialex.gmail.com>
2023-06-19 14:22:25 -0700

    Rectangle class that inherits from Base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+'''
+    Rectangle class
+'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''
+        Rectangle class
+    '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''
+        Return a private attribute
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+        Set value for private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+        Return a private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+        Setter for private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+        Return private attribute
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+        Setter for private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+        Return private atriibute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+        Setter for private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
+
+    @staticmethod
+    def setter_validation(attr, value):
+        '''
+        Validates setter attributes
+        '''
+        if not isinstance(value, int):
+            raise TypeError(f"{attr} must be an integer")
+
+        if attr == "width" or attr == "height":
+            if value <= 0:
+                raise ValueError(f"{attr} must be > 0")
+        elif value < 0:
+            raise ValueError(f"{attr} must be >= 0")
