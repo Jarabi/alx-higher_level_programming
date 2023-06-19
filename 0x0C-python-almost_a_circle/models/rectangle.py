151c6e1415af7323dc56f1d726ba0fe44d5a93d3
Alex Jarabi <jarabialex.gmail.com>
2023-06-19 14:27:11 -0700

    Added valiation for setter methods to Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 '''
-    Rectangle class
+Rectangle class
 '''
 from models.base import Base
 
 
 class Rectangle(Base):
     '''
-        Rectangle class
+    Rectangle class
     '''
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
