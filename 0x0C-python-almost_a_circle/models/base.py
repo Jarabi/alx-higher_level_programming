b89f2e37b8601d7fd06d51917f678c4e6f3830e5
Alex Jarabi <jarabialex.gmail.com>
2023-06-19 14:01:52 -0700

    The first Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
             id: public instance attribute
         '''
         if id is not None:
-                self.id = id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
