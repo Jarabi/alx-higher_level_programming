24ccc51c325b007d953399e63be838b89e651b10
Alex Jarabi <jarabialex@gmail.com>
2023-07-18 20:16:11 +0300

    Adding a script that lists all rows of the table first_table from the database hbtn_0c_0.

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
--- a script that lists all rows of the table first_table from the database hbtn_0c_0.
---- USAGE: cat 6-list_values.sql | mysql -hlocalhost -uroot -p <database>
+-- List all rows in a table
 SELECT * FROM first_table;
