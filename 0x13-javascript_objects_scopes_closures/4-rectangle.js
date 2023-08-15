b10f40050986fbd54d7e865c5b42e17d4d5556c2
Alex Jarabi <jarabialex@gmail.com>
2023-08-15 21:15:02 +0000

    Adding a class Rectangle that defines a rectangle.

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // prints the rectangle using 'X'
+  print () {
+    for (let r = 0; r < this.height; r++) {
+      let row = '';
+      for (let c = 0; c < this.width; c++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  // swaps values of width and height
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // Multiplies width and height by 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
