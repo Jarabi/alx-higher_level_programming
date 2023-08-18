f27be02879b7fe499873a43e6617e0a1a4437f46
Alex Jarabi <jarabialex@gmail.com>
2023-08-18 15:46:02 +0000

    Adding a script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-import sys
+'''
+Lists all states from the database hbtn_0e_0_usa
+'''
+
+from sys import argv
 import MySQLdb
 
 
-def get_states(username, password, db_name):
-    '''
-    Lists all states from the database hbtn_0e_0_usa
-    '''
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
     conn = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -18,14 +22,8 @@ def get_states(username, password, db_name):
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
     cur.close()
     conn.close()
-
-
-if __name__ == "__main__":
-    u = sys.argv[1]
-    p = sys.argv[2]
-    d = sys.argv[3]
-    get_states(u, p, d)
