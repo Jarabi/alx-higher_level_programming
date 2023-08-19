918f98aab076d5b60751639945b6534bd8e3e0d6
Alex Jarabi <jarabialex@gmail.com>
2023-08-19 08:40:31 +0000

    Adding a script that takes in an argument and displays all values where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+This script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    argument = argv[4]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+    )
+
+    cur = connection.cursor()
+    query = """SELECT * FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC;
+            """
+    format_query = query.format(argument)
+    cur.execute(format_query)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    connection.close()
