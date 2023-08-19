c61eddf56025df7f8f5255754921060c04377e1e
Alex Jarabi <jarabialex@gmail.com>
2023-08-19 17:06:41 +0000

    Adding a script that takes in the name of a state as an argument and lists all cities of that state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    argument = argv[4]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+    )
+
+    cursor = connection.cursor()
+    query = """SELECT name FROM cities
+            WHERE cities.state_id = (
+                SELECT id FROM states
+                WHERE name = %s
+            ) ORDER BY cities.id ASC"""
+
+    cursor.execute(query, (argument,))
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        for item in row:
+            print(item, end=" ")
+    print()
+
+    cursor.close()
+    connection.close()
