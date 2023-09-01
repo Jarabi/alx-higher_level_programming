487ad85ed7db95fb1e96a33f910fdc5e8fc69e36
Alex Jarabi <jarabialex@gmail.com>
2023-09-01 23:24:48 +0000

    Adding Python script that sends an email in a POST request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    email = argv[2]
+    payload = {'email': argv[2]}
 
-    res = requests.post(url, data={'email', email})
+    res = requests.post(url, data=payload)
     print(res.text)
