dba6fcbb837012362440b237ca69fa1e71f94f97
Alex Jarabi <jarabialex@gmail.com>
2023-09-03 16:36:46 +0000

    Adding a script that causes the server to respond with a message: You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to URL that causes the server to respond with a message
-curl -sfw "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin:School" -d "user_id=98" 0.0.0.0:5000/catch_me_2
